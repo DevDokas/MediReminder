2346be24a24da01e2edf0c03aa7f8fea99a92739
Igor Gomes <iguinhogms@gmail.com>
2023-06-06 21:26:05 -0300

    initializing api connection

--- src/app/leaflet/page.tsx
+++ src/app/leaflet/page.tsx
@@ -1,11 +1,35 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+import axios from 'axios';
 
 export default function LeafletPage(): any {
-  useEffect(() => {}, []);
+  const [searchMedicine, setSearchMedicine] = useState<string>();
+  const [resultMedicine, setResultMedicine] = useState<any>({});
+
+  function FetchAPI(): void {
+    axios
+      .get(`https://bula.vercel.app/pesquisar?nome=${searchMedicine}`)
+      .then((res) => {
+        setResultMedicine(res.data.content);
+        console.log(resultMedicine[0].nomeProduto);
+      })
+      .catch((res) => {
+        console.log(res);
+      });
+  }
 
   return (
     <>
-      <h1>Leaflet</h1>
+      <div>
+        <h1>{searchMedicine || 'Leaflet'}</h1>
+        <input
+          type="text"
+          onChange={(e) => {
+            setSearchMedicine(e.target.value);
+          }}
+        />
+        <button onClick={FetchAPI}>Buscar</button>
+      </div>
     </>
   );
 }
